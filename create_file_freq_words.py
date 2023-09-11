0b6e449231b1573dbd1e121f536f2022d19a7d35
André Coêlho <andrelucascoelho281@gmail.com>
2023-09-11 20:12:16 -0300

    Delete data.db

--- create_file_freq_words.py
+++ create_file_freq_words.py
@@ -1,33 +1,24 @@
 import csv
 import json
 
-# Função para ler um vetor de um arquivo CSV
 def read_vector(file_name):
     with open(file_name, "r", encoding="utf-8") as file:
         csv_reader = csv.reader(file, delimiter=",")
-        vector = next(csv_reader)  # Lê a próxima linha do CSV
+        vector = next(csv_reader)
     return vector
 
-# Função para escrever dados em um arquivo JSON
 def write_json(file_name, data):
     with open(file_name, "w", encoding="utf-8") as file:
-        json.dump(data, file, indent=6, ensure_ascii=False)  # Escreve os dados em formato JSON
+        json.dump(data, file, indent=6, ensure_ascii=False)
 
-# Função para escrever dados em um arquivo
 def write_file(file_name, data):
     with open(file_name, "w", encoding="utf-8") as file:
-        file.writelines([l + ",\n" for l in data])  # Escreve as linhas de dados no arquivo
+        file.writelines([l + ",\n" for l in data])
 
-# Lê os vetores de palavras e frequências de arquivos CSV
 words = read_vector("words.csv")
 freq = read_vector("freq.csv")
-
-# Convertendo as frequências para uma lista de inteiros
 freq = list(map(int, freq))
-
-# Imprime o valor máximo de frequência (para verificar)
 print(max(freq))
-
 # words_freq = dict(zip(words, freq))
 # write_json("wordsFreq.json", words_freq)
 # write_json("words.js", words)
--- init_db.py
+++ init_db.py
@@ -4,14 +4,13 @@ import numpy as np
 
 from numpy import array
 
-# Cria uma tabela chamada "embedding" e uma tabela "tests" no banco de dados "data.db"
 def create_word2vec_table():
     con = sqlite3.connect("data.db")
-    con.execute("PRAGMA journal_mode=WAL")  # Define o modo de jornal para WAL (Write-Ahead Logging)
+    con.execute("PRAGMA journal_mode=WAL")
     cur = con.cursor()
     cur.execute(
         """CREATE TABLE IF NOT EXISTS embedding (word TEXT PRIMARY KEY, vector BLOB)"""
-    )  # Cria a tabela "embedding" com colunas "word" (texto) e "vector" (BLOB)
+    )
     cur.execute(
         """CREATE TABLE IF NOT EXISTS tests (id INTEGER NOT NULL,
                                             date DATETIME NOT NULL,
@@ -19,37 +18,35 @@ def create_word2vec_table():
                                             palavra_sonda TEXT NOT NULL,
                                             palavra_respondida TEXT,
                                             similaridade FLOAT NOT NULL)"""
-    )  # Cria a tabela "tests" com colunas para armazenar informações de testes
-    con.commit()  # Confirma as alterações no banco de dados
-    con.close()  # Fecha a conexão com o banco de dados
+    )
+    con.commit()
+    con.close()
 
-# Faz upload dos vetores de palavras do arquivo "glove_s100.txt" para o banco de dados "data.db"
 def upload_embedding():
     con = sqlite3.connect("data.db")
-    con.execute("PRAGMA journal_mode=WAL")  # Define o modo de jornal para WAL
+    con.execute("PRAGMA journal_mode=WAL")
     cur = con.cursor()
-    con.execute("DELETE FROM embedding")  # Remove os dados existentes da tabela "embedding"
+    con.execute("DELETE FROM embedding")
     with open("glove_s100.txt", "r", encoding="utf-8") as w2v_file:
-        _ = w2v_file.readline()  # Lê a primeira linha (cabeçalho) do arquivo
+        _ = w2v_file.readline()
         n = 0
         for line in w2v_file:
-            words = line.rstrip().split(" ")  # Divide a linha em palavras
-            word = words[0]  # A primeira palavra é a palavra em si
-            vector = array([float(w) for w in words[1:]])  # Vetor de floats após a primeira palavra
+            words = line.rstrip().split(" ")
+            word = words[0]
+            vector = array([float(w) for w in words[1:]])
             cur.execute(
                 """INSERT INTO embedding VALUES (?, ?)""", (word, pickle.dumps(vector))
-            )  # Insere a palavra e seu vetor na tabela "embedding"
+            )
             n += 1
             if n % 100000 == 0:
-                print(f"processed {n} (+1) lines")  # Mostra o progresso a cada 100.000 linhas
-                con.commit()  # Confirma as alterações no banco de dados
-    con.commit()  # Confirma as alterações finais no banco de dados
-    con.close()  # Fecha a conexão com o banco de dados
+                print(f"processed {n} (+1) lines")
+                con.commit()
+    con.commit()
+    con.close()
 
 def main():
-    create_word2vec_table()  # Chama a função para criar a tabela no banco de dados
-    # upload_embedding()  # Chama a função para fazer upload dos vetores das palavras
+    create_word2vec_table()
+    # upload_embedding()
 
 if __name__ == "__main__":
-    main()  # Chama a função principal apenas se o arquivo for executado diretamente
-    
+    main()
